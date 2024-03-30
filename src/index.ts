202348d873dce7915c91c5ab87c37c9d9c0eb683
vincentferraro <v.ferraro@live.fr>
2024-03-30 12:10:56 -0400

    creating streaming video

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world')
+import  express from 'express';
+
+const app = express()
+const port = 3000;
+
+import path from 'path'
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,'welcome.html'))
+});
+
+app.get('/video',)
+
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port} to: http://localhost:${port}/`)
+});
+
